85dd963ee32d8cba73975826ad4eecdfcb6344dd
shrinivasshweta <41103798+shrinivasshweta@users.noreply.github.com>
2018-10-11 02:47:48 +0530

    Added comments

--- BuzzupRsvpService/src/main/java/com/stackroute/buzzup/repository/EventRepository.java
+++ BuzzupRsvpService/src/main/java/com/stackroute/buzzup/repository/EventRepository.java
@@ -1,17 +1,13 @@
 package com.stackroute.buzzup.repository;
-
-//import java.util.List;
-
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.stackroute.buzzup.model.EventDetails;
+//This repository is used to perform various operations on MongoDB	
 
 @Repository
 public interface EventRepository extends MongoRepository<EventDetails, String> {
 
-//	public List<EventDetails> getByEmailId(String emailId);
-	
 	public EventDetails getByEventName(String eventName);
 	public EventDetails getByEmailId(String emailId);
 	
